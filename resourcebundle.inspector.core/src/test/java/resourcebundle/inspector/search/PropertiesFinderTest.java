159776084dcf4fc4b978b889c1a26c6151e5d865
astrapi69 <asterios.raptis@gmx.net>
2014-04-15 21:09:08 +0200

    unit test for properties finder.

---
+++ resourcebundle.inspector.core/src/test/java/resourcebundle/inspector/search/PropertiesFinderTest.java
@@ -0,0 +1,28 @@
+package resourcebundle.inspector.search;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+import net.sourceforge.jaulp.file.search.PathFinder;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+public class PropertiesFinderTest {
+
+	@Test
+	public void testFind() throws IOException {
+		File rootDir = PathFinder.getSrcTestResourcesDir();
+		PropertiesFinder finder = new PropertiesFinder(rootDir);
+		finder.find();
+		Map<File, String> foundedfiles = finder.getPropertiesToLocale();
+		File dir = PathFinder.getSrcTestResourcesDir();
+		final String propertiesFilename = "resources.properties";
+		File propertiesFile = new File(dir, propertiesFilename);
+		String actual = foundedfiles.get(propertiesFile);
+		String expected = "default";
+		AssertJUnit.assertTrue(actual.equals(expected));
+	}
+
+}
