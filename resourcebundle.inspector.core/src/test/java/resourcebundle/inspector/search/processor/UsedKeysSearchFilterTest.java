23e1ffc5315a4227e7f79bfe6acf19a2ab1f1d88
astrapi69 <asterios.raptis@gmx.net>
2014-10-04 16:37:55 +0200

    extended docs.

--- resourcebundle.inspector.core/src/test/java/resourcebundle/inspector/search/processor/UsedKeysSearchFilterTest.java
+++ resourcebundle.inspector.core/src/test/java/resourcebundle/inspector/search/processor/UsedKeysSearchFilterTest.java
@@ -46,19 +46,19 @@ public class UsedKeysSearchFilterTest {
 	protected KeySearchModel newKeySearchModel() {
 		KeySearchModel model = new KeySearchModel();
 		Properties properties = new Properties();
-
+		// We set the properties that are supposed readed from a properties file.
 		properties.put("com.example.gui.window.title", "Hello, there!");
 		properties.put("com.example.gui.window.buttons.ok", "OK");
 		properties.put("com.example.gui.window.buttons.cancel", "Cancel");
 		properties.put("com.example.gui", "Cancel");
 		model.setBase(properties);
-
+		// Set the project directory as search path...
 		File projectdir = PathFinder.getProjectDirectory();
 		model.setSearchDir(projectdir);
-
+		// We want to search only java files so we set the extension...
 		String[] fileExtensions = { ".java" };
 		model.setFileExtensions(fileExtensions);
-
+		// We can set the files that shell be excuded from the search...
 		Set<File> exclude = new HashSet<File>();
 		File ex = new File(PathFinder.getSrcTestJavaDir(),
 				"/resourcebundle/inspector/search/processor/UsedKeysSearchFilterTest.java");
