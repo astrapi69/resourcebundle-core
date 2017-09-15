45110cabb5e746a3c381fff4884e48ee31894dff
Asterios Raptis <asterios.raptis@gmx.net>
2017-09-15 16:04:45 +0200

    format and sort members

--- resourcebundle-inspector/src/test/java/de/alpharogroup/resourcebundle/locale/LocaleExtensionsTest.java
+++ resourcebundle-inspector/src/test/java/de/alpharogroup/resourcebundle/locale/LocaleExtensionsTest.java
@@ -39,9 +39,47 @@ import lombok.extern.slf4j.Slf4j;
  * {@link LocaleExtensions}.
  */
 @Slf4j
-@ExtensionMethod({LocaleExtensions.class})
+@ExtensionMethod({ LocaleExtensions.class })
 public class LocaleExtensionsTest
 {
+
+	@Test
+	public void testContains()
+	{
+		boolean condition = LocaleExtensions.contains(Locales.GREEK);
+		assertTrue(condition);
+
+		condition = LocaleExtensions.contains(Locales.HELLENIC);
+		assertTrue(condition);
+
+		Locale locale = Locales.HELLENIC;
+
+		final String englishName = locale.getDisplayName(Locale.ENGLISH);
+		final String country = locale.getCountry();
+		String iSO3Country = "";
+		try
+		{
+			iSO3Country = locale.getISO3Country();
+		}
+		catch (final Exception e)
+		{
+			log.error(e.getClass().getName() + ": " + e.getMessage());
+		}
+		final String englishCountryName = locale.getDisplayCountry(Locale.ENGLISH);
+
+		final String language = locale.getLanguage();
+		final String iSO3Language = locale.getISO3Language();
+		final String englishLanguageName = locale.getDisplayLanguage(Locale.ENGLISH);
+
+		final String script = locale.getScript();
+		final String englishScript = locale.getDisplayScript(Locale.ENGLISH);
+
+		System.out.printf(
+			"Name: %s%n" + "Country: %s; %s - %s%n" + "" + "Language: %s; %s - %s%n"
+				+ "Script: %s - %s%n",
+			englishName, country, iSO3Country, englishCountryName, language, iSO3Language,
+			englishLanguageName, script, englishScript);
+	}
 
 	/**
 	 * Test method for {@link LocaleExtensions#getLocaleFilenameSuffix(Locale)}
@@ -168,44 +206,6 @@ public class LocaleExtensionsTest
 		expected = "el_GR";
 		actual = LocaleExtensions.getLocaleName(Locales.HELLENIC);
 		AssertJUnit.assertEquals(expected, actual);
-	}
-
-	@Test
-	public void testContains()
-	{
-		boolean condition = LocaleExtensions.contains(Locales.GREEK);
-		assertTrue(condition);
-
-		condition = LocaleExtensions.contains(Locales.HELLENIC);
-		assertTrue(condition);
-
-		Locale locale = Locales.HELLENIC;
-
-		final String englishName = locale.getDisplayName(Locale.ENGLISH);
-		final String country = locale.getCountry();
-		String iSO3Country = "";
-		try
-		{
-			iSO3Country = locale.getISO3Country();
-		}
-		catch (final Exception e)
-		{
-			log.error(e.getClass().getName() + ": " + e.getMessage());
-		}
-		final String englishCountryName = locale.getDisplayCountry(Locale.ENGLISH);
-
-		final String language = locale.getLanguage();
-		final String iSO3Language = locale.getISO3Language();
-		final String englishLanguageName = locale.getDisplayLanguage(Locale.ENGLISH);
-
-		final String script = locale.getScript();
-		final String englishScript = locale.getDisplayScript(Locale.ENGLISH);
-
-		System.out.printf(
-			"Name: %s%n" + "Country: %s; %s - %s%n" + "" + "Language: %s; %s - %s%n"
-				+ "Script: %s - %s%n",
-			englishName, country, iSO3Country, englishCountryName, language, iSO3Language,
-			englishLanguageName, script, englishScript);
 	}
 
 }
