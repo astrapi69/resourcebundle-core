8d15a73e16241b0d7eb063fb1ca4a4dd8c03c6f3
Asterios Raptis <asteri.raptis@gmail.com>
2024-07-19 16:38:18 +0200

    formatted with spotless

--- src/main/java/io/github/astrapi69/resourcebundle/file/namefilter/package-info.java
+++ src/main/java/io/github/astrapi69/resourcebundle/file/namefilter/package-info.java
@@ -1,4 +1,5 @@
 /**
- * This package provides specific classes for properties an xml file names that implements the inteface java.io.FilenameFilter
+ * This package provides specific classes for properties an xml file names that implements the
+ * inteface java.io.FilenameFilter
  */
 package io.github.astrapi69.resourcebundle.file.namefilter;
--- src/main/java/io/github/astrapi69/resourcebundle/inspector/search/processor/package-info.java
+++ src/main/java/io/github/astrapi69/resourcebundle/inspector/search/processor/package-info.java
@@ -1,4 +1,5 @@
 /**
- * This package provides classes and intefaces for the processing the search and inspection of a properties files
+ * This package provides classes and intefaces for the processing the search and inspection of a
+ * properties files
  */
 package io.github.astrapi69.resourcebundle.inspector.search.processor;
